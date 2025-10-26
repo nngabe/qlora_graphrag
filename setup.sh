7dd31b0100ded86bd819d19bdf2c16ab7130cdbd
nngabe <nickgabriel8@gmail.com>
2025-10-26 18:01:36 +0000

    updated config and setup

--- setup.sh
+++ setup.sh
@@ -4,10 +4,14 @@
 #
 # git clone https://github.com/nngabe/qlora_graphrag.git
 # cd qlora_graphrag/
+#source /opt/miniforge3/etc/profile.d/conda.sh
 
-conda init
+rm -rf gretriever/
+git clone https://github.com/nngabe/gretriever.git
+
 conda create -n panther python=3.11
 conda activate panther
 python -m pip install -r requirements.txt 
+python -m pip install -U transformers peft accelerate bitsandbytes
 
 sed -i '7c from langchain_text_splitters import RecursiveCharacterTextSplitter' /venv/panther/lib/python3.11/site-packages/stark_qa/tools/process_text.py
